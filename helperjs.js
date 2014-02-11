83b1befdd513f4fc692ad9ea4b17d4f272c33a9f
agamemnus <agamemnus@gmail.com>
2014-02-11 16:58:51 -0500

    Update helperjs.js

--- helperjs.js
+++ helperjs.js
@@ -1,5 +1,5 @@
 // http://jsfiddle.net/brigand/U8Y6C/ ?
-// HelperJS version 3.6.
+// HelperJS version 3.5.
 // Easter Egg in plain sight: (thanks to Brigand)
 // function foo(){return XII}fooFixed=new Function(foo.toString().replace(/function\s*\w+\(\)\s*{/,"").slice(0,-1).replace(/[IVXLCDM]+/g,function(a){for(k=d=l=0;i={I:1,V:5,X:10,L:50,C:100,D:500,M:1E3}[a[k++]];l=i)d+=i>l?i-2*l:i;return d})); fooFixed()
 
@@ -3752,12 +3752,12 @@ function on_background_image_url_load (current_div, callback) {
 // Creates a solid one color copy (plus transparency) of the source image in a <canvas> object with the specified rgb colors.
 // Each pixel will receive this color if it does not have an alpha value of 0;
 // the alpha value of the color would then be set to 255.
-function create_single_color_canvas_copy (source, r, g, b, unit_style_size) {
+function create_single_color_canvas_copy (source, r, g, b, unit_style_size, width, height) {
  if (typeof r == "undefined") r = 0
  if (typeof g == "undefined") g = 0
  if (typeof b == "undefined") b = 0
- if (source.style.width == null) {var source_width = source.width} else {var source_width = parseFloat(source.style.width)*unit_style_size}
- if (source.style.height == null) {var source_height = source.height} else {var source_height = parseFloat(source.style.height)*unit_style_size}
+ if (source.style.width  == null) {var source_width  = source.width } else {var source_width  = parseFloat(source.style.width)  * unit_style_size}
+ if (source.style.height == null) {var source_height = source.height} else {var source_height = parseFloat(source.style.height) * unit_style_size}
  
  var target = document.createElement ('canvas')
  var ctx_target = target.getContext('2d')
@@ -3779,7 +3779,7 @@ function create_single_color_canvas_copy (source, r, g, b, unit_style_size) {
    imagedata_target_data[i + 2] = 0
   }
  }
- ctx_target.putImageData(imagedata_target, 0, 0)
+ ctx_target.putImageData (imagedata_target, 0, 0)
  return target
 }
 // </Graphics/image/canvas functions.>
